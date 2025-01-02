e1ba656284d0991e60c2490dcdec837b06c61fc0
NotKingDuck <133243846+NotKingDuck@users.noreply.github.com>
2025-01-02 15:04:04 +0100

    Update PyChat.py

--- PyChat.py
+++ PyChat.py
@@ -1,3 +1,5 @@
 import time
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 print("I'll make it better and more secure, I just don't want you guys to use my current mess :)")
 time.sleep(10)
